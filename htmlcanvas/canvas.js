af1b60670629026fc94bf2c5782c4fffcafff3de
abhisheksuryavanshi <ab524189@gmail.com>
2020-06-17 15:30:18 +0530

    moving circles added

--- htmlcanvas/canvas.js
+++ htmlcanvas/canvas.js
@@ -15,56 +15,86 @@ var c = canvas.getContext("2d");
 var i, j;
 var flag = false;
 var height = 0;
-// for(i=0;i<8;i++){
-// 	if(flag){
-// 		start = 100;
-// 	}
-// 	else{
-// 		start = 0;
-// 	}
-// 	flag = !flag;
-// 	for(j=0;j<8;j++){
-// 		c.fillRect(start,height,100,100);
-// 		start = start + 200;
-// 	}
-// 	height = height + 100;
+
+class Circle {
+    constructor(x, y, dx, dy, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.color = color;
+        console.log(
+            "x = " + this.x + "radius = " + this.radius + "dx = " + this.dx
+        );
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, flag);
+        flag = !flag;
+        c.strokeStyle = "black";
+        c.stroke();
+        c.fillStyle = this.color;
+        c.fill();
+    }
+    update() {
+        if (
+            this.x + this.radius >= window.innerWidth ||
+            this.x - this.radius <= 0
+        ) {
+            this.dx = -1 * this.dx;
+        }
+        if (
+            this.y + this.radius >= window.innerHeight ||
+            this.y - this.radius <= 0
+        ) {
+            this.dy = -1 * this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+    }
+}
+
+// function Circle(x, y, dx, dy, radius) {
+//     this.x = x;
+//     this.y = y;
+//     this.dx = x;
+//     this.dy = y;
+//     this.radius = radius;
+
+//     this.draw() = function () {
+//         c.beginPath();
+//         c.arc(x, y, r, 0, Math.PI * 2, flag);
+//         flag = !flag;
+//         c.strokeStyle = color;
+//         c.stroke();
+//     };
 // }
 
-// var numCircles = 200;
-// for (i=0;i<numCircles;i++) {
-// 	var x = Math.random()*window.innerWidth;
-// 	var y = Math.random()*window.innerHeight;
-// 	c.beginPath();
-// 	c.arc(x,y,30,0,Math.PI*2,flag);
-// 	flag = !flag;
-// 	c.strokeStyle=getRandomColor();
-// 	c.stroke();
-// }
-
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var r = 50;
-var color = getRandomColor();
-var dx = (Math.random() - 0.5) * 10;
-var dy = (Math.random() - 0.5) * 10;
+var circleArray = [];
+var num_circles = 70;
+for (i = 0; i < num_circles; i++) {
+    var r = 50;
+    var x = Math.random() * (innerWidth - r);
+    while (x - r <= 0) {
+        x = Math.random() * (innerWidth - r);
+    }
+    var y = Math.random() * (innerHeight - r);
+    while (y - r <= 0) {
+        y = Math.random() * (innerHeight - r);
+    }
+    var dx = (Math.random() - 0.5) * 10;
+    var dy = (Math.random() - 0.5) * 10;
+    circleArray.push(new Circle(x, y, dx, dy, r, getRandomColor()));
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, y, r, 0, Math.PI * 2, flag);
-    flag = !flag;
-    c.strokeStyle = color;
-    c.stroke();
-
-    if (x + r >= innerWidth || x - r <= 0) {
-        dx = -1 * dx;
+    for (i = 0; i < num_circles; i++) {
+        circleArray[i].update();
     }
-    if (y + r >= innerHeight || y - r <= 0) {
-        dy = -1 * dy;
-    }
-    x += dx;
-    y += dy;
 }
 
 animate();
